aa1224939846a6c7294a106565a22366f6cc344c
Eva8210 <ceglik.evelina@gmail.com>
2024-01-30 12:27:03 +0200

    pirmas blynas

---
+++ ToDo-list/src/test/java/lt/techin/evelina/test/ToDoListTest.java
@@ -0,0 +1,9 @@
+package lt.techin.evelina.test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ToDoListTest {
+
+    WebDriver driver = new ChromeDriver();
+}
