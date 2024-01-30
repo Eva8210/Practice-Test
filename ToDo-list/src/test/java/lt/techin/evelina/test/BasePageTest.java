329e5574794e1bbf286ccb121e3a18ed9c343a22
Eva8210 <ceglik.evelina@gmail.com>
2024-01-30 16:02:05 +0200

    almost done, needs fixing

--- ToDo-list/src/test/java/lt/techin/evelina/test/BasePageTest.java
+++ ToDo-list/src/test/java/lt/techin/evelina/test/BasePageTest.java
@@ -18,8 +18,8 @@ public class BasePageTest {
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
     }
-//    @AfterEach
-//    void teardown() {
-//        driver.quit();
-//    }
+    @AfterEach
+    void teardown() {
+        driver.quit();
+    }
 }
