70552b3c259e5a81769f4025958974bb3746b701
Eva8210 <ceglik.evelina@gmail.com>
2024-01-30 16:01:10 +0200

    almost done, needs fixing

--- ToDo-list/src/test/java/lt/techin/evelina/test/ToDoListTest.java
+++ ToDo-list/src/test/java/lt/techin/evelina/test/ToDoListTest.java
@@ -1,9 +1,37 @@
 package lt.techin.evelina.test;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class ToDoListTest {
+import java.time.Duration;
 
-    WebDriver driver = new ChromeDriver();
+public class ToDoListTest extends BasePageTest {
+
+//@FindBy(xpath = "//input[contains(@placeholder, 'add new todo')]")
+//WebElement inputTodo;
+
+
+    @Test
+    void addNewTodo() {
+        WebElement inputTodo = driver.findElement(By.xpath("//*[@id=\"container\"]/input"));
+        inputTodo.sendKeys("Get a job", Keys.ENTER);
+        WebElement lastInput = driver.findElement(By.cssSelector("#container > ul > li:last-child"));
+        lastInput.getText();
+        System.out.println(lastInput.getText());
+        Assertions.assertEquals("Get a job",lastInput.getText());
+        lastInput.click();
+        inputTodo.getAttribute("completed");
+
+    }
+
 }
