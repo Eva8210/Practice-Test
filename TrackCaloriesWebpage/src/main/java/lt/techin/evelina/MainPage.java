69406d19e8fbe492cfb5b91b16caee2ca4bb86e9
Eva8210 <ceglik.evelina@gmail.com>
2024-02-08 09:02:46 +0200

    New exercise structure added

---
+++ TrackCaloriesWebpage/src/main/java/lt/techin/evelina/MainPage.java
@@ -0,0 +1,9 @@
+package lt.techin.evelina;
+
+import org.openqa.selenium.WebDriver;
+
+public class MainPage extends BasePage{
+    public MainPage(WebDriver driver) {
+        super(driver);
+    }
+}
---
+++ TrackCaloriesWebpage/src/test/java/lt/techin/evelina/test/BasePageTest.java
@@ -0,0 +1,25 @@
+package lt.techin.evelina.test;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class BasePageTest {
+
+    WebDriver driver;
+
+    @BeforeEach
+    void setup() {
+        driver = new ChromeDriver();
+        driver.get("https://practice.expandtesting.com/tracalorie/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+    }
+//    @AfterEach
+//    void teardown() {
+//        driver.quit();
+//    }
+
+}
