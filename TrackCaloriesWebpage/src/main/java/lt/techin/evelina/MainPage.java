4dfb8cddf8794bcd1228c314f4624fbc16868d94
Eva8210 <ceglik.evelina@gmail.com>
2024-02-08 14:18:11 +0200

    add meal tested, calories counted

--- TrackCaloriesWebpage/src/main/java/lt/techin/evelina/MainPage.java
+++ TrackCaloriesWebpage/src/main/java/lt/techin/evelina/MainPage.java
@@ -1,9 +1,56 @@
 package lt.techin.evelina;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class MainPage extends BasePage{
     public MainPage(WebDriver driver) {
         super(driver);
+    }
+
+    @FindBy(xpath = "//input[@id='item-name']")
+    WebElement addItemInput;
+    @FindBy(xpath = "//input[@id='item-calories']")
+    WebElement addCaloriesInput;
+    @FindBy(xpath = "//button[contains(text(), 'Add Meal')]")
+    WebElement addMealButton;
+    @FindBy(xpath = "//li[contains(@class,'collection-item')]")
+    List<WebElement> meals;
+
+    public void addNewItem(String item) {
+        addItemInput.sendKeys(item);
+    }
+    public void addNewCalories(int calories){
+        addCaloriesInput.sendKeys(String.valueOf(calories));
+    }
+    public void clickButtonAddMeal() {
+        addMealButton.click();
+    }
+    public void enterMealAndAddToList(String item, int calories){
+        addNewItem(item);
+        addNewCalories(calories);
+        clickButtonAddMeal();
+    }
+    public ArrayList<String> createMealsList() {
+        ArrayList<String> mealsList = new ArrayList<>(meals.stream().map(WebElement::getText).collect(Collectors.toList()));
+        return mealsList;
+    }
+
+    public int caloriesCalculator() {
+
+        List<Integer> allCalories = new ArrayList<>();
+        for (WebElement meal : meals) {
+            String text = meal.getText();
+            String[] parts = text.split(": ");
+            String numericPart = parts[1].replace("Calories", "").trim();
+            int calorieValue = Integer.parseInt(numericPart);
+            allCalories.add(calorieValue);
+        }
+        return allCalories.stream().mapToInt(Integer::intValue).sum();
     }
 }
--- TrackCaloriesWebpage/src/test/java/lt/techin/evelina/test/MainPageTest.java
+++ TrackCaloriesWebpage/src/test/java/lt/techin/evelina/test/MainPageTest.java
@@ -1,4 +1,40 @@
 package lt.techin.evelina.test;
 
+import lt.techin.evelina.MainPage;
+import org.junit.jupiter.api.Test;
+
 public class MainPageTest extends BasePageTest{
+
+    @Test
+    void addItemToTheList() {
+        MainPage mainPage = new MainPage(driver);
+
+        String newItem = "bread";
+        int newCalories = 600;
+        mainPage.addNewItem(newItem);
+        mainPage.addNewCalories(newCalories);
+        mainPage.clickButtonAddMeal();
+
+        mainPage.enterMealAndAddToList("Egg", 50);
+        mainPage.enterMealAndAddToList("Yogurt", 87);
+        mainPage.enterMealAndAddToList("Pancakes", 369);
+        mainPage.enterMealAndAddToList("Bacon", 200);
+        mainPage.enterMealAndAddToList("Coffee", 47);
+        System.out.println(mainPage.createMealsList());
+
+    }
+    @Test
+    void CalculateMealsCalories() {
+        MainPage mainPage = new MainPage(driver);
+
+        mainPage.enterMealAndAddToList("Egg", 50);
+        mainPage.enterMealAndAddToList("Yogurt", 87);
+        mainPage.enterMealAndAddToList("Pancakes", 369);
+        mainPage.enterMealAndAddToList("Bacon", 200);
+        mainPage.enterMealAndAddToList("Coffee", 47);
+        System.out.println(mainPage.createMealsList());
+
+        System.out.println(mainPage.caloriesCalculator());
+
+        }
 }
