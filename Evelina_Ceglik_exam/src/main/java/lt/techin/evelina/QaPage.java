4f4ae109faaac14b1c52771464b85370b49b45cb
Eva8210 <ceglik.evelina@gmail.com>
2024-02-09 11:26:26 +0200

    adding new tests

--- Evelina_Ceglik_exam/src/main/java/lt/techin/evelina/QaPage.java
+++ Evelina_Ceglik_exam/src/main/java/lt/techin/evelina/QaPage.java
@@ -4,7 +4,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class QaPage extends BasePage{
     public QaPage(WebDriver driver) {
@@ -14,7 +16,11 @@ public class QaPage extends BasePage{
     WebElement buttonShowItemsAsList;
 
     @FindBy(xpath = "//div[@id='product-list']//div[@class='product-thumb']")
-    List<WebElement> itemsList;
+    List<WebElement> items;
+    @FindBy(xpath = "//div[@id='product-list']//div[@class='product-thumb']")
+    WebElement  requiredItem;
+    @FindBy(xpath = "//div[@id='product-list']//a[text()='Code Confusion Raccoon']")
+    List<WebElement> itemsNames;
 
     public void clickButtonShowItemsAsList(){
         buttonShowItemsAsList.click();
@@ -22,5 +28,33 @@ public class QaPage extends BasePage{
     public boolean isActiveButtonShowItemsAsList() {
         return buttonShowItemsAsList.getAttribute("class").contains("active");
     }
-    public void 
+    public ArrayList<String> putItemsIntoList() {
+        ArrayList<String> itemsList = new ArrayList<>(items.stream().map(WebElement::getText).collect(Collectors.toList()));
+        return itemsList;
+    }
+
+    public boolean checkIfListContainsSpecialItem(String productName){
+        for (String specialName : putItemsIntoList()) {
+            if (specialName.contains(productName)){
+                return true;
+            }
+        }
+        return false;
+    }
+    public boolean checkIfRequiredItemIsDisplayed(String productName) {
+        if (requiredItem.getText().contains(productName)) {
+            return requiredItem.isDisplayed();
+        }
+        return false;
+    }
+    public boolean checkIfRequiredItemNameMatches(String productName) {
+        ArrayList<String> itemsNamesList = new ArrayList<>(itemsNames.stream().map(WebElement::getText).collect(Collectors.toList()));
+        for (String specialName : itemsNamesList) {
+            if (specialName.equals(productName)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
 }
--- Evelina_Ceglik_exam/src/test/java/lt/techin/evelina/test/QaPageTest.java
+++ Evelina_Ceglik_exam/src/test/java/lt/techin/evelina/test/QaPageTest.java
@@ -18,7 +18,21 @@ public class QaPageTest extends BasePageTest{
     }
     @Test
     void testIfItemsAreInTheList(){
+        MainPage mainPage = new MainPage(driver);
+        mainPage.clickButtonQa();
+        QaPage qaPage = new QaPage(driver);
+        qaPage.clickButtonShowItemsAsList();
+        qaPage.putItemsIntoList();
+        System.out.println(qaPage.putItemsIntoList());
+        String productName = "Code Confusion Raccoon";
+        Assertions.assertTrue(qaPage.checkIfListContainsSpecialItem(productName), productName + " does not exist in the shop");
 
+        qaPage.checkIfRequiredItemIsDisplayed(productName);
+        System.out.println(qaPage.checkIfRequiredItemIsDisplayed(productName));
+        Assertions.assertTrue(qaPage.checkIfRequiredItemIsDisplayed(productName), "Given product is not displayed");
+
+        System.out.println(qaPage.checkIfRequiredItemNameMatches(productName));
+        Assertions.assertTrue(qaPage.checkIfRequiredItemNameMatches(productName), "Item name does not match");
     }
 }
 
